9e467bdeae1f0b6eeeb34602c14ca3d5ab9af23b
JongeunKeum <jon9eun@gmail.com>
2021-06-17 20:38:30 +0900

    thread.c using pthread functions (dining_philo())

---
+++ philo/srcs/thread.c
@@ -0,0 +1,36 @@
+#include "philo.h"
+
+int	philo(t_philo *philo)
+{
+	
+}
+
+int	monitor(t_philo *philo)
+{
+
+}
+
+int	dining_philo(t_info *info)
+{
+	int	i;
+
+	info->base_time = get_time();
+	i = -1;
+	while (++i < info->num_philo)
+	{
+		info->philo[i].start_time = get_time();
+		if (pthread_create(&(info->philo[i].philo_th), NULL, philo, &(info->philo[i])))
+			return (str_err("Failed to create thread.\n"));
+		if (pthread_create(&(info->philo[i].monitor), NULL, monitor, &(info->philo[i])))
+			return (str_err("Failed to create thread.\n"));
+	}
+	i = -1;
+	while (++i < info->num_philo)
+	{
+		if (pthread_join(info->philo[i].philo_th, NULL))
+			return (str_err("Failed to join thread.\n"));
+		if (pthread_join(info->philo[i].monitor, NULL))
+			return (str_err("Failed to join thread.\n"));
+	}
+	return (0);
+}
