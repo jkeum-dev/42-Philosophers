524e2cd35fd03f74e22bfb22d44e32318163e9c3
JongeunKeum <jon9eun@gmail.com>
2021-06-17 17:55:08 +0900

    add get_time()

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -4,24 +4,16 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
-# include <pthread.h>
+# include <string.h>
 # include <sys/time.h>
+# include "structures.h"
 
 # define MAX_INT 2147483647
 
-typedef struct	s_info	t_info;
-
-struct s_info
-{
-	int	num_philo;
-	int	time_die;
-	int	time_eat;
-	int	time_sleep;
-	int	num_must_eat;
-};
-
 int	str_err(char *str);
 int	ft_strlen(char *str);
+int	get_time(void);
+
 int	ft_atoi_pos(char *arg);
 
 # endif
