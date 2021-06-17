eff4cff1f099d947c336ce102fbd2dc9fede6348
JongeunKeum <jon9eun@gmail.com>
2021-06-17 17:55:47 +0900

    initialize structure info & philo

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -1,21 +1,66 @@
 #include "philo.h"
 
-int	get_base_info(t_info *info, char **argv, int argc)
+int	init_fork(t_info *info)
+{
+	int	i;
+
+	info->fork = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * info->num_philo);
+	if (!info->fork)
+		return (str_err("Failed to allocate memory.\n"));
+	i = -1;
+	while (++i < info->num_philo)
+	{
+		if (pthread_mutex_init(&info->fork[i], NULL))
+			return (str_err("Failed to initialize mutex.\n"));
+	}
+}
+
+int	init_info(t_info *info, char **argv, int argc)
 {
 	info->num_philo = ft_atoi_pos(argv[1]);
 	if (info->num_philo <= 0)
 		return (str_err("The number of philosophers must be at least one.\n"));
 	else if (info->num_philo > 200)
-		return (str_err("Do not test with more than 200 philosophers\n"));
+		return (str_err("Do not test with more than 200 philosophers.\n"));
 	info->time_die = ft_atoi_pos(argv[2]);
 	info->time_eat = ft_atoi_pos(argv[3]);
 	info->time_sleep = ft_atoi_pos(argv[4]);
 	if (info->time_die < 60 || info->time_eat < 60 || info->time_sleep < 60)
-		return (str_err("Do not test under 60ms\n"));
+		return (str_err("Do not test under 60ms.\n"));
 	if (argc == 6)
+	{
 		info->num_must_eat = ft_atoi_pos(argv[5]);
+		if (info->num_must_eat <= 0)
+			return (str_err("Each philosopher must eat at least once.\n"));
+	}
 	else
 		info->num_must_eat = -1;
+	info->stop = 0;
+	info->base_time = 0;
+	if (init_fork(info))
+		return (1);	
+	if (pthread_mutex_init(&info->status, NULL))
+		return (str_err("Failed to initialize mutex.\n"));
+	return (0);
+}
+
+int	init_philo(t_info *info)
+{
+	int	i;
+
+	info->philo = (t_philo *)malloc(sizeof(t_philo) * info->num_philo);
+	if (!info->philo)
+		return (str_err("Failed to allocate memory.\n"));
+	i = -1;
+	while (++i < info->num_philo)
+	{
+		info->philo[i].n = i;
+		info->philo[i].fork_l = i;
+		info->philo[i].fork_r = (i + 1) % info->num_philo;
+		info->philo[i].start_time = 0;
+		info->philo[i].meals = 0;
+		info->philo[i].info = info;
+	}
 	return (0);
 }
 
@@ -25,6 +70,10 @@ int	main(int argc, char *argv[])
 
 	if (argc < 5 || argc > 6)
 		return (str_err("Error: argument\n"));
-	if (get_base_info(&info, argv, argc))
+	memset(&info, 0, sizeof(t_info));
+	if (init_info(&info, argv, argc))
 		return (1);
+	if (init_philo(&info))
+		return (1);
+	dining_philo(&info);
 }
