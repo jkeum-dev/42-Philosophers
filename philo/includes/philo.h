9bf36c08d15a1f76c4316b99da88338aec078846
JongeunKeum <jon9eun@gmail.com>
2021-06-18 16:16:55 +0900

    add status flags to print

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -10,12 +10,23 @@
 
 # define MAX_INT 2147483647
 
-int	str_err(char *str);
-int	ft_strlen(char *str);
-int	get_time(void);
+# define FORK 1
+# define EATING 2
+# define SLEEPING 3
+# define THINKING 4
+# define DIED 5
 
-int	ft_atoi_pos(char *arg);
+int		str_err(char *str);
+int		ft_strlen(char *str);
+int		get_time(void);
 
-int	dining_philo(t_info *info);
+int		ft_atoi_pos(char *arg);
 
-# endif
+int		dining_philo(t_info *info);
+void	eating(t_philo *philo);
+void	sleeping(t_philo *philo);
+void	thinking(t_philo *philo);
+
+void	print_status(t_philo *philo, int status);
+
+#endif
