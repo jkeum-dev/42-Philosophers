53e8fded8de139542800ad45fb8d3d00eb9637be
JongeunKeum <jon9eun@gmail.com>
2021-06-18 21:43:27 +0900

    deadlock solving

--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -1,43 +1,62 @@
 #include "philo.h"
 
-void	take_fork(t_philo *philo)
+int	take_fork(t_philo *philo)
 {
-	if (philo->n % 2 == 0)
+	// if (philo->n % 2 == 0)
+	// {
+	// 	pthread_mutex_lock(&(philo->info->fork[philo->fork_r]));
+	// 	print_status(philo, FORK);
+	// 	pthread_mutex_lock(&(philo->info->fork[philo->fork_l]));
+	// 	print_status(philo, FORK);
+	// }
+	// else
+	// {
+	// 	pthread_mutex_lock(&(philo->info->fork[philo->fork_l]));
+	// 	print_status(philo, FORK);
+	// 	pthread_mutex_lock(&(philo->info->fork[philo->fork_r]));
+	// 	print_status(philo, FORK);
+	// }
+	if (philo->info->starv[philo->fork_r] == 0 && philo->info->starv[philo->fork_l] == 0)
 	{
+		philo->info->starv[philo->fork_r] = 1;
+		philo->info->starv[philo->fork_l] = 1;
 		pthread_mutex_lock(&(philo->info->fork[philo->fork_r]));
 		print_status(philo, FORK);
 		pthread_mutex_lock(&(philo->info->fork[philo->fork_l]));
 		print_status(philo, FORK);
+		return (0);
 	}
 	else
-	{
-		pthread_mutex_lock(&(philo->info->fork[philo->fork_l]));
-		print_status(philo, FORK);
-		pthread_mutex_lock(&(philo->info->fork[philo->fork_r]));
-		print_status(philo, FORK);
-	}
+		return (1);
 }
 
-void	eating(t_philo *philo)
+int	eating(t_philo *philo)
 {
 	int	i;
 
-	take_fork(philo);
+	if (take_fork(philo))
+		return (1);
 	print_status(philo, EATING);
+	philo->start_time = get_time();
 	i = -1;
 	while (++i < philo->info->time_eat && !philo->info->stop)
 		usleep(1000);
 	philo->meals++;
-	if (philo->n % 2 == 0)
-	{
-		pthread_mutex_unlock(&(philo->info->fork[philo->fork_r]));
-		pthread_mutex_unlock(&(philo->info->fork[philo->fork_l]));
-	}
-	else
-	{
-		pthread_mutex_unlock(&(philo->info->fork[philo->fork_l]));
-		pthread_mutex_unlock(&(philo->info->fork[philo->fork_r]));
-	}
+	// if (philo->n % 2 == 0)
+	// {
+	// 	pthread_mutex_unlock(&(philo->info->fork[philo->fork_r]));
+	// 	pthread_mutex_unlock(&(philo->info->fork[philo->fork_l]));
+	// }
+	// else
+	// {
+	// 	pthread_mutex_unlock(&(philo->info->fork[philo->fork_l]));
+	// 	pthread_mutex_unlock(&(philo->info->fork[philo->fork_r]));
+	// }
+	pthread_mutex_unlock(&(philo->info->fork[philo->fork_r]));
+	pthread_mutex_unlock(&(philo->info->fork[philo->fork_l]));
+	philo->info->starv[philo->fork_r] = 0;
+	philo->info->starv[philo->fork_l] = 0;
+	return (0);
 }
 
 void	sleeping(t_philo *philo)
