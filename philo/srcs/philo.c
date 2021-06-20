1933aaeccf70773d68bf428095806743df1b3fae
JongeunKeum <jon9eun@gmail.com>
2021-06-20 20:09:46 +0900

    fix error when num_philo is 1

--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -1,66 +1,32 @@
 #include "philo.h"
 
-void	take_fork(t_philo *philo)
-{
-	// if (philo->n % 2 == 0)
-	// {
-	// 	pthread_mutex_lock(&(philo->info->fork[philo->fork_r]));
-	// 	print_status(philo, FORK);
-	// 	pthread_mutex_lock(&(philo->info->fork[philo->fork_l]));
-	// 	print_status(philo, FORK);
-	// }
-	// else
-	// {
-	// 	pthread_mutex_lock(&(philo->info->fork[philo->fork_l]));
-	// 	print_status(philo, FORK);
-	// 	pthread_mutex_lock(&(philo->info->fork[philo->fork_r]));
-	// 	print_status(philo, FORK);
-	// }
-	// if (philo->info->starv[philo->fork_r] == 0 && philo->info->starv[philo->fork_l] == 0)
-	// {
-	// 	philo->info->starv[philo->fork_r] = 1;
-	// 	philo->info->starv[philo->fork_l] = 1;
-	// 	pthread_mutex_lock(&(philo->info->fork[philo->fork_r]));
-	// 	print_status(philo, FORK);
-	// 	pthread_mutex_lock(&(philo->info->fork[philo->fork_l]));
-	// 	print_status(philo, FORK);
-	// 	return (0);
-	// }
-	// else
-	// 	return (1);
-
-	pthread_mutex_lock(&(philo->info->fork[philo->fork_r]));
-	print_status(philo, FORK);
-	pthread_mutex_lock(&(philo->info->fork[philo->fork_l]));
-	print_status(philo, FORK);
-}
-
 void	eating(t_philo *philo)
 {
-	// if (take_fork(philo))
-	// 	return (1);
-	take_fork(philo);
-	print_status(philo, EATING);
-	philo->start_time = get_time();
-	while (get_time() - philo->start_time <= philo->info->time_eat && \
-	!philo->info->stop)
-		usleep(1000);
-	philo->meals++;
-	// if (philo->n % 2 == 0)
-	// {
-	// 	pthread_mutex_unlock(&(philo->info->fork[philo->fork_r]));
-	// 	pthread_mutex_unlock(&(philo->info->fork[philo->fork_l]));
-	// }
-	// else
-	// {
-	// 	pthread_mutex_unlock(&(philo->info->fork[philo->fork_l]));
-	// 	pthread_mutex_unlock(&(philo->info->fork[philo->fork_r]));
-	// }
-	pthread_mutex_unlock(&(philo->info->fork[philo->fork_r]));
-	pthread_mutex_unlock(&(philo->info->fork[philo->fork_l]));
-	// philo->info->starv[philo->fork_r] = 0;
-	// philo->info->starv[philo->fork_l] = 0;
-	// return (0);
+	if (philo->info->num_philo == 1)
+	{
+		pthread_mutex_lock(&philo->info->fork[philo->fork_r]);
+		print_status(philo, FORK);
+		while (!philo->info->stop)
+			usleep(1000);
+		pthread_mutex_unlock(&philo->info->fork[philo->fork_r]);
+	}
+	else
+	{
+		pthread_mutex_lock(&philo->info->fork[philo->fork_r]);
+		print_status(philo, FORK);
+		pthread_mutex_lock(&philo->info->fork[philo->fork_l]);
+		print_status(philo, FORK);
+		pthread_mutex_lock(&philo->protect);
+		print_status(philo, EATING);
+		philo->start_time = get_time();
+		pthread_mutex_unlock(&philo->protect);
+		while (get_time() - philo->start_time <= philo->info->time_eat && \
+		!philo->info->stop)
+			usleep(1000);
+		philo->meals++;
+		pthread_mutex_unlock(&philo->info->fork[philo->fork_r]);
+		pthread_mutex_unlock(&philo->info->fork[philo->fork_l]);
+	}
 }
 
 void	sleeping(t_philo *philo)
