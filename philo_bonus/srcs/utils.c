eb9d8eadc6e7fc4f173cdbd12f87a8b049fc46a1
JongeunKeum <jon9eun@gmail.com>
2021-06-21 21:00:44 +0900

    utils.c bonus

---
+++ philo_bonus/srcs/utils.c
@@ -0,0 +1,27 @@
+#include "philo_bonus.h"
+
+int	str_err(char *str)
+{
+	write(2, str, ft_strlen(str));
+	return (1);
+}
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	if (!str)
+		return (0);
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+int	get_time(void)
+{
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
+}
