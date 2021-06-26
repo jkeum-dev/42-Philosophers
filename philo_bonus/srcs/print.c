f6db346e72edfd4a02db4c44963bd3977778b191
JongeunKeum <jon9eun@gmail.com>
2021-06-27 01:06:50 +0900

    add print.c (print_status())

---
+++ philo_bonus/srcs/print.c
@@ -0,0 +1,24 @@
+#include "philo_bonus.h"
+
+void	print_status(t_philo *philo, int status)
+{
+	sem_wait(philo->info->status);
+	if (philo->info->stop)
+	{
+		sem_post(philo->info->status);
+		return ;
+	}
+	printf("%d\t", get_time() - philo->info->base_time);
+	printf("Philo %d ", philo->n + 1);
+	if (status == FORK)
+		printf("has taken a fork\n");
+	else if (status == EATING)
+		printf("is eating\n");
+	else if (status == SLEEPING)
+		printf("is sleeping\n");
+	else if (status == THINKING)
+		printf("is thinking\n");
+	else if (status == DIED)
+		printf("died\n");
+	sem_post(philo->info->status);
+}
