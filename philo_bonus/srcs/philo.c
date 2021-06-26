86b018be4aa5168aa136fbc9e9cf04e6f9a382c7
JongeunKeum <jon9eun@gmail.com>
2021-06-27 01:07:33 +0900

    eating, sleeping, thinking

--- philo_bonus/srcs/philo.c
+++ philo_bonus/srcs/philo.c
@@ -8,5 +8,28 @@ void	eating(t_philo *philo)
 	print_status(philo, FORK);
 	sem_wait(philo->protect);
 	print_status(philo, EATING);
-	
+	philo->start_time = get_time();
+	sem_post(philo->protect);
+	while (get_time() - philo->start_time <= philo->info->time_eat && \
+	!philo->info->stop)
+		usleep(1000);
+	philo->meals++;
+	sem_post(philo->info->fork);
+	sem_post(philo->info->fork);
+}
+
+void	sleeping(t_philo *philo)
+{
+	int	start_sleep;
+
+	start_sleep = get_time();
+	print_status(philo, SLEEPING);
+	while (get_time() - start_sleep <= philo->info->time_sleep && \
+	!philo->info->stop)
+		usleep(1000);
+}
+
+void	thinking(t_philo *philo)
+{
+	print_status(philo, THINKING);
 }
