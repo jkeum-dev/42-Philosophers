f99e06c234bb89b2ef6148f3e498de43def42f5c
JongeunKeum <jon9eun@gmail.com>
2021-06-27 23:10:09 +0900

    add some tool functions, free_all()

--- philo_bonus/srcs/main.c
+++ philo_bonus/srcs/main.c
@@ -1,7 +1,21 @@
 #include "philo_bonus.h"
-#include <fcntl.h>
 
-int	init_info(t_info *info, char **argv, int argc)
+int		ft_pthread_init(pthread_t pth, void *func, void *param)
+{
+	if (pthread_create(&pth, NULL, func, param))
+		return (str_err("Failed to create thread.\n"));
+	if (pthread_detach(pth))
+		return (str_err("Failed to detach thread.\n"));
+	return (0);
+}
+
+sem_t	*ft_sem_unlink_open(char *name, int value)
+{
+	sem_unlink(name);
+	return (sem_open(name, O_CREAT | O_EXCL, 0644, value));
+}
+
+int		init_info(t_info *info, char **argv, int argc)
 {
 	info->num_philo = ft_atoi_pos(argv[1]);
 	if (info->num_philo <= 0)
@@ -22,15 +36,15 @@ int	init_info(t_info *info, char **argv, int argc)
 	else
 		info->num_must_eat = -1;
 	info->stop = 0;
-	info->base_time = 0;
-	info->fork = sem_open("/fork", O_CREAT | O_EXCL, 0644, info->num_philo);
-	info->status = sem_open("/status", O_CREAT | O_EXCL, 0644, 1);
-	info->full = sem_open("/full", O_CREAT | O_EXCL, 0644, 0);
-	info->died = sem_open("/died", O_CREAT | O_EXCL, 0644, 0);
+	info->all_full = 0;
+	info->fork = ft_sem_unlink_open("/fork", info->num_philo);
+	info->status = ft_sem_unlink_open("/status", 1);
+	info->full = ft_sem_unlink_open("/full", 0);
+	info->died = ft_sem_unlink_open("/died", 0);
 	return (0);
 }
 
-int	init_philo(t_info *info)
+int		init_philo(t_info *info)
 {
 	int		i;
 	char	*name;
@@ -46,13 +60,13 @@ int	init_philo(t_info *info)
 		info->philo[i].meals = 0;
 		info->philo[i].info = info;
 		name = ft_strjoin_free("/protect", ft_itoa(i + 1));
-		info->philo[i].protect = sem_open(name, O_CREAT | O_EXCL, 0644, 0);
+		info->philo[i].protect = ft_sem_unlink_open(name, 1);
 		free(name);
 	}
 	return (0);
 }
 
-int	main(int argc, char *argv[])
+int		main(int argc, char *argv[])
 {
 	t_info	info;
 
@@ -63,16 +77,12 @@ int	main(int argc, char *argv[])
 		return (1);
 	if (init_philo(&info))
 		return (1);
-	/*
-	**	make philo processes (1 ~ num_philo)
-	**	each process eating/sleeping/thinking
-	**	check someone died
-	**	check everyone full (num_must_eat)
-	*/
 	if (dining_philo(&info))
 	{
-		// free_all
+		free_all(&info);
 		return (1);
 	}
+	free_all(&info);
+	// system("leaks philo_bonus");
 	return (0);
 }
